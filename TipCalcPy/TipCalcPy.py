c7059a4e03447cebf4946f7c82c0e54b4ab6bb6a
KDUser <128939316+KDUser12@users.noreply.github.com>
2023-08-16 17:59:24 +0200

    Update TipCalcPy.py

--- TipCalcPy/TipCalcPy.py
+++ TipCalcPy/TipCalcPy.py
@@ -1,25 +1,39 @@
+# Fonction pour saisir les données de manière répétée jusqu'à ce qu'elles soient valides
+# noinspection PyShadowingNames
 def data_entry():
-    total_invoice = input("Veuillez saisir le montant total de la facture : ")
-    tip_percentage = input("Veuillez saisir le pourcentage de pourboire que vous souhaitez donner : ")
+    while True:
+        total_invoice = input("Veuillez saisir le montant total de la facture : ")
+        tip_percentage = input("Veuillez saisir le pourcentage de pourboire que vous souhaitez donner : ")
 
-    try:
-        total_invoice = int(total_invoice)
-        tip_percentage = int(tip_percentage)
-    except ValueError:
-        print("Une erreur est survenue, les valeurs que vous avez indiquées ne sont pas bonnes veuillez réessayer.")
-        data_entry()
-    else:
-        return total_invoice, tip_percentage
+        try:
+            total_invoice = float(total_invoice)
+            tip_percentage = float(tip_percentage)
+            return total_invoice, tip_percentage
+        except ValueError:
+            print(
+                "Une erreur est survenue, les valeurs que vous avez indiquées ne sont pas bonnes. Veuillez réessayer.")
 
 
-def tip_calculation(total_invoice, tip_percentage):
-    tip_amount = total_invoice * (tip_percentage / 100)
+# Fonction pour afficher les résultats
+# noinspection PyShadowingNames
+def display_results(tip_amount, total_amount_payable):
     print("Montant du pourboire : {}".format(tip_amount))
-    
+    print("Montant total à payer : {}".format(total_amount_payable))
 
 
 if __name__ == "__main__":
     print("Bienvenue dans TipCalcPy - Calculatrice de Pourboire !")
-    total_invoice, tip_percentage = data_entry()
-    tip_calculation(total_invoice, tip_percentage)
 
+    # Saisie des données
+    total_invoice, tip_percentage = data_entry()
+
+    # Calcul du pourboire
+    tip_amount = total_invoice * (tip_percentage / 100)
+
+    # Calcul du montant total
+    total_amount_payable = total_invoice + tip_amount
+
+    # Affichage des résultats
+    display_results(tip_amount, total_amount_payable)
+
+    input("Appuyez sur entrer pour quitter le programme.")
