4674e88f7a6e421823b1abac814fe2ff5a45e150
KDUser <128939316+KDUser12@users.noreply.github.com>
2023-08-16 17:37:41 +0200

    Update TipCalcPy.py

---
+++ TipCalcPy/TipCalcPy.py
@@ -0,0 +1,24 @@
+def data_entry():
+    total_invoice = input("Veuillez saisir le montant total de la facture : ")
+    tip_percentage = input("Veuillez saisir le pourcentage de pourboire que vous souhaitez donner : ")
+
+    try:
+        total_invoice = int(total_invoice)
+        tip_percentage = int(tip_percentage)
+    except ValueError:
+        print("Une erreur est survenue, les valeurs que vous avez indiquées ne sont pas bonnes veuillez réessayer.")
+        data_entry()
+    else:
+        return total_invoice, tip_percentage
+
+
+def tip_calculation(total_invoice, tip_percentage):
+    result = total_invoice * (tip_percentage / 100)
+    print(result)
+
+
+if __name__ == "__main__":
+    print("Bienvenue dans TipCalcPy - Calculatrice de Pourboire !")
+    total_invoice, tip_percentage = data_entry()
+    tip_calculation(total_invoice, tip_percentage)
+
